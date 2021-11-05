796c60a4801dab39015ee75834fd4cb07272bdec
JoeDarcy <59360012+JoeDarcy@users.noreply.github.com>
2021-11-05 14:47:00 +0000

    Both ships building but controlled by the same buttons currently. Need to separate them out.

--- Build_them_Bad_GJ/Assets/Scripts/ButtonSelection.cs
+++ Build_them_Bad_GJ/Assets/Scripts/ButtonSelection.cs
@@ -4,9 +4,23 @@ using UnityEngine;
 
 public class ButtonSelection : MonoBehaviour
 {
+    // Hull selection bools
     public static bool hull_1_selected = false;
     public static bool hull_2_selected = false;
     public static bool hull_3_selected = false;
+
+    // Hull selection bools
+    public static bool shield_1_selected = false;
+    public static bool shield_2_selected = false;
+    public static bool shield_3_selected = false;
+
+    // Hull selection bools
+    public static bool weapon_1_selected = false;
+    public static bool weapon_2_selected = false;
+    public static bool weapon_3_selected = false;
+
+
+    // Hull selection button functions
     public void Hull_1_button() {
         hull_1_selected = true;
         hull_2_selected = false;
@@ -23,5 +37,43 @@ public class ButtonSelection : MonoBehaviour
         hull_3_selected = true;
         hull_1_selected = false;
         hull_2_selected = false;
+    }
+
+    // Shield selection button functions
+    public void shield_1_button() {
+	    shield_1_selected = true;
+        shield_2_selected = false;
+        shield_3_selected = false;
+    }
+
+    public void shield_2_button() {
+	    shield_2_selected = true;
+	    shield_1_selected = false;
+	    shield_3_selected = false;
+    }
+
+    public void shield_3_button() {
+	    shield_3_selected = true;
+	    shield_1_selected = false;
+	    shield_2_selected = false;
+    }
+
+    // Weapon selection button functions
+    public void weapon_1_button() {
+	    weapon_1_selected = true;
+	    weapon_2_selected = false;
+	    weapon_3_selected = false;
+    }
+
+    public void weapon_2_button() {
+	    weapon_2_selected = true;
+	    weapon_1_selected = false;
+	    weapon_3_selected = false;
+    }
+
+    public void weapon_3_button() {
+	    weapon_3_selected = true;
+	    weapon_1_selected = false;
+	    weapon_2_selected = false;
     }
 }
--- Build_them_Bad_GJ/Assets/Scripts/ShipBuilding.cs
+++ Build_them_Bad_GJ/Assets/Scripts/ShipBuilding.cs
@@ -29,6 +29,12 @@ public class ShipBuilding : MonoBehaviour
     [SerializeField] private GameObject special_2 = null;
     [SerializeField] private GameObject special_3 = null;
 
+    // Total ship strength
+    public static int totalShipStrength = 0;
+    private int hullStrength = 0;
+    private int shieldStrength = 0;
+    private int weaponStrength = 0;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -57,14 +63,55 @@ public class ShipBuilding : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Output total ship stregnth
+        Debug.Log("Total Ship Strength: " + totalShipStrength);
+        Debug.Log("Hull Strength: " + hullStrength);
+        Debug.Log("Shield Strength: " + shieldStrength);
+        Debug.Log("Weapon Strength: " + weaponStrength);
+
+        // Update total ship strength based on choice of ship components
+        totalShipStrength = hullStrength + shieldStrength + weaponStrength;
+
+        // Hull strength
+	    if (ButtonSelection.hull_1_selected == true) {
+		    hullStrength = 1;
+	    }
+	    if (ButtonSelection.hull_2_selected == true) {
+		    hullStrength = 2;
+	    }
+	    if (ButtonSelection.hull_3_selected == true) {
+		    hullStrength = 3;
+	    }
+
+	    // Shield strength
+	    if (ButtonSelection.shield_1_selected == true) {
+		    shieldStrength = 1;
+	    }
+	    if (ButtonSelection.shield_2_selected == true) {
+		    shieldStrength = 2;
+	    }
+	    if (ButtonSelection.shield_3_selected == true) {
+		    shieldStrength = 3;
+	    }
+
+	    // Weapon strength
+	    if (ButtonSelection.weapon_1_selected == true) {
+		    weaponStrength = 1;
+	    }
+	    if (ButtonSelection.weapon_2_selected == true) {
+		    weaponStrength = 2;
+	    }
+	    if (ButtonSelection.weapon_3_selected == true) {
+		    weaponStrength = 3;
+	    }
+
+
         // Hull selection
         // Hull 1
-	    if (ButtonSelection.hull_1_selected == true)
-	    {
+        if (ButtonSelection.hull_1_selected == true) {
 		    hull_1.SetActive(true);
 	    }
-	    else
-	    {
+	    else {
 		    hull_1.SetActive(false);
 	    }
 	    // Hull 2
@@ -79,5 +126,45 @@ public class ShipBuilding : MonoBehaviour
 	    } else {
 		    hull_3.SetActive(false);
 	    }
+
+        // Shield selection
+        // Shield 1
+        if (ButtonSelection.shield_1_selected == true) {
+	        shield_1.SetActive(true);
+        } else {
+	        shield_1.SetActive(false);
+        }
+        // Shield 2
+        if (ButtonSelection.shield_2_selected == true) {
+	        shield_2.SetActive(true);
+        } else {
+	        shield_2.SetActive(false);
+        }
+        // Shield 3
+        if (ButtonSelection.shield_3_selected == true) {
+	        shield_3.SetActive(true);
+        } else {
+	        shield_3.SetActive(false);
+        }
+
+        // Weapon selection
+        // Weapon 1
+        if (ButtonSelection.weapon_1_selected == true) {
+	        weapon_1.SetActive(true);
+        } else {
+	        weapon_1.SetActive(false);
+        }
+        // Weapon 2
+        if (ButtonSelection.weapon_2_selected == true) {
+	        weapon_2.SetActive(true);
+        } else {
+	        weapon_2.SetActive(false);
+        }
+        // Weapon 3
+        if (ButtonSelection.weapon_3_selected == true) {
+	        weapon_3.SetActive(true);
+        } else {
+	        weapon_3.SetActive(false);
+        }
     }
 }
