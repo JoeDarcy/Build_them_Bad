d95d932aba8716b80653d7448bb1da94eba2d869
JoeDarcy <59360012+JoeDarcy@users.noreply.github.com>
2021-11-05 12:51:28 +0000

    Project setup.

---
+++ Build_them_Bad_GJ/Assets/Scripts/ShipBuilding.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipBuilding : MonoBehaviour
+{
+    // Hull types
+    [SerializeField] private GameObject hull_1 = null;
+    [SerializeField] private GameObject hull_2 = null;
+    [SerializeField] private GameObject hull_3 = null;
+
+    // Shield types
+    [SerializeField] private GameObject shield_1 = null;
+    [SerializeField] private GameObject shield_2 = null;
+    [SerializeField] private GameObject shield_3 = null;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
