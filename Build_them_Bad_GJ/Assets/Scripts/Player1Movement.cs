674489045d90fbda3d98601c709f47d7fbd4b70f
JoeDarcy <59360012+JoeDarcy@users.noreply.github.com>
2021-11-06 16:20:38 +0000

    Two player movement sorted! Feeling fine!!!

---
+++ Build_them_Bad_GJ/Assets/Scripts/Player1Movement.cs
@@ -0,0 +1,134 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player1Movement : MonoBehaviour
+{
+	[SerializeField] private float pos_X_ShipSpeed = 0.0f;
+	[SerializeField] private float neg_X_ShipSpeed = 0.0f;
+	[SerializeField] private float pos_Y_ShipSpeed = 0.0f;
+	[SerializeField] private float neg_Y_ShipSpeed = 0.0f;
+	[SerializeField] private float maxSpeed = 0.0f;
+	[SerializeField] private float accelerationSpeed = 0.0f;
+	[SerializeField] private float declerationSpeed = 0.0f;
+
+	[SerializeField] private string right = null;
+	[SerializeField] private string left = null;
+	[SerializeField] private string up = null;
+	[SerializeField] private string down = null;
+
+	private bool rightPressed = false;
+	private bool leftPressed = false;
+	private bool upPressed = false;
+	private bool downPressed = false;
+
+	// Start is called before the first frame update
+	void Start()
+    {
+	    pos_X_ShipSpeed = 0.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+		// Right pressed
+	    if (Input.GetKeyDown(right))
+	    {
+			rightPressed = true;
+	    }
+		// Right released
+	    if (Input.GetKeyUp(right)) {
+			rightPressed = false;
+	    }
+
+	    // Left pressed
+	    if (Input.GetKeyDown(left)) {
+		    leftPressed = true;
+	    }
+	    // Left released
+	    if (Input.GetKeyUp(left)) {
+		    leftPressed = false;
+	    }
+
+	    // Up pressed
+	    if (Input.GetKeyDown(up)) {
+		    upPressed = true;
+	    }
+	    // Up released
+	    if (Input.GetKeyUp(up)) {
+		    upPressed = false;
+	    }
+
+	    // Down pressed
+	    if (Input.GetKeyDown(down)) {
+		    downPressed = true;
+	    }
+	    // Down released
+	    if (Input.GetKeyUp(down)) {
+		    downPressed = false;
+	    }
+
+		// Right acceleration
+		if (rightPressed == true)
+	    {
+		    if (pos_X_ShipSpeed <= maxSpeed) {
+			    pos_X_ShipSpeed += accelerationSpeed;
+		    }
+		}
+	    if (rightPressed == false) {
+			if (pos_X_ShipSpeed > 0) {
+				pos_X_ShipSpeed -= declerationSpeed;
+			} else if (pos_X_ShipSpeed < 0)
+			{
+				pos_X_ShipSpeed = 0;
+			}
+		}
+
+		// Left acceleration
+		if (leftPressed == true)
+		{
+			if (neg_X_ShipSpeed >= -maxSpeed) {
+				neg_X_ShipSpeed -= accelerationSpeed;
+			}
+		}
+		if (leftPressed == false) {
+			if (neg_X_ShipSpeed < 0) {
+				neg_X_ShipSpeed += declerationSpeed;
+			} else if (neg_X_ShipSpeed > 0) {
+				neg_X_ShipSpeed = 0;
+			}
+		}
+
+		// Up acceleration
+		if (upPressed == true) {
+			if (pos_Y_ShipSpeed <= maxSpeed) {
+				pos_Y_ShipSpeed += accelerationSpeed;
+			}
+		}
+		if (upPressed == false) {
+			if (pos_Y_ShipSpeed > 0) {
+				pos_Y_ShipSpeed -= declerationSpeed;
+			} else if (pos_Y_ShipSpeed < 0) {
+				pos_Y_ShipSpeed = 0;
+			}
+		}
+
+		// Down acceleration
+		if (downPressed == true) {
+			if (neg_Y_ShipSpeed >= -maxSpeed) {
+				neg_Y_ShipSpeed -= accelerationSpeed;
+			}
+		}
+		if (downPressed == false) {
+			if (neg_Y_ShipSpeed < 0) {
+				neg_Y_ShipSpeed += declerationSpeed;
+			} else if (neg_Y_ShipSpeed > 0) {
+				neg_Y_ShipSpeed = 0;
+			}
+		}
+
+		// Movement
+		transform.position = new Vector3(transform.position.x + ((pos_X_ShipSpeed / 10) + (neg_X_ShipSpeed / 10)), transform.position.y + ((pos_Y_ShipSpeed / 10) + (neg_Y_ShipSpeed / 10)), transform.position.z);
+    }
+}
--- Build_them_Bad_GJ/Assets/Scripts/Player2_Movement.cs
+++ Build_them_Bad_GJ/Assets/Scripts/Player2_Movement.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Cube : MonoBehaviour
+public class Player2_Movement : MonoBehaviour
 {
 	[SerializeField] private float pos_X_ShipSpeed = 0.0f;
 	[SerializeField] private float neg_X_ShipSpeed = 0.0f;
@@ -11,88 +11,77 @@ public class Cube : MonoBehaviour
 	[SerializeField] private float maxSpeed = 0.0f;
 	[SerializeField] private float accelerationSpeed = 0.0f;
 	[SerializeField] private float declerationSpeed = 0.0f;
-
+	/*
 	[SerializeField] private string right = null;
 	[SerializeField] private string left = null;
 	[SerializeField] private string up = null;
 	[SerializeField] private string down = null;
-
+	*/
 	private bool rightPressed = false;
 	private bool leftPressed = false;
 	private bool upPressed = false;
 	private bool downPressed = false;
 
 	// Start is called before the first frame update
-	void Start()
-    {
-	    pos_X_ShipSpeed = 0.0f;
-    }
+	void Start() {
+		pos_X_ShipSpeed = 0.0f;
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-		// Check input type
-		if (up == "up")
-		{
-			up = "KeyCode.UpArrow";
-		}
+	// Update is called once per frame
+	void Update() {
 
 		// Right pressed
-	    if (Input.GetKeyDown(right))
-	    {
+		if (Input.GetKeyDown(KeyCode.RightArrow)) {
 			rightPressed = true;
-	    }
+		}
 		// Right released
-	    if (Input.GetKeyUp(right)) {
+		if (Input.GetKeyUp(KeyCode.RightArrow)) {
 			rightPressed = false;
-	    }
+		}
 
-	    // Left pressed
-	    if (Input.GetKeyDown(left)) {
-		    leftPressed = true;
-	    }
-	    // Left released
-	    if (Input.GetKeyUp(left)) {
-		    leftPressed = false;
-	    }
+		// Left pressed
+		if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+			leftPressed = true;
+		}
+		// Left released
+		if (Input.GetKeyUp(KeyCode.LeftArrow)) {
+			leftPressed = false;
+		}
 
-	    // Up pressed
-	    if (Input.GetKeyDown(up)) {
-		    upPressed = true;
-	    }
-	    // Up released
-	    if (Input.GetKeyUp(up)) {
-		    upPressed = false;
-	    }
+		// Up pressed
+		if (Input.GetKeyDown(KeyCode.UpArrow)) {
+			upPressed = true;
+		}
+		// Up released
+		if (Input.GetKeyUp(KeyCode.UpArrow)) {
+			upPressed = false;
+		}
 
-	    // Down pressed
-	    if (Input.GetKeyDown(down)) {
-		    downPressed = true;
-	    }
-	    // Down released
-	    if (Input.GetKeyUp(down)) {
-		    downPressed = false;
-	    }
+		// Down pressed
+		if (Input.GetKeyDown(KeyCode.DownArrow)) {
+			downPressed = true;
+		}
+		// Down released
+		if (Input.GetKeyUp(KeyCode.DownArrow)) {
+			downPressed = false;
+		}
 
 		// Right acceleration
-		if (rightPressed == true)
-	    {
-		    if (pos_X_ShipSpeed <= maxSpeed) {
-			    pos_X_ShipSpeed += accelerationSpeed;
-		    }
+		if (rightPressed == true) {
+			if (pos_X_ShipSpeed <= maxSpeed) {
+				pos_X_ShipSpeed += accelerationSpeed;
+			}
 		}
-	    if (rightPressed == false) {
+		if (rightPressed == false) {
 			if (pos_X_ShipSpeed > 0) {
 				pos_X_ShipSpeed -= declerationSpeed;
-			} else if (pos_X_ShipSpeed < 0)
-			{
+			} else if (pos_X_ShipSpeed < 0) {
 				pos_X_ShipSpeed = 0;
 			}
 		}
 
 		// Left acceleration
-		if (leftPressed == true)
-		{
+		if (leftPressed == true) {
 			if (neg_X_ShipSpeed >= -maxSpeed) {
 				neg_X_ShipSpeed -= accelerationSpeed;
 			}
@@ -135,5 +124,5 @@ public class Cube : MonoBehaviour
 
 		// Movement
 		transform.position = new Vector3(transform.position.x + ((pos_X_ShipSpeed / 10) + (neg_X_ShipSpeed / 10)), transform.position.y + ((pos_Y_ShipSpeed / 10) + (neg_Y_ShipSpeed / 10)), transform.position.z);
-    }
+	}
 }
